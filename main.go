240727419836128439415df7abfcce70bdafd417
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-10-30 19:52:24 +0100

    add CreateGrayImage() + wait group

--- main.go
+++ main.go
@@ -8,28 +8,27 @@ import (
 	"image/png"
 	"os"
 	"runtime"
+	"sync"
 	"time"
 )
 
-// var wg sync.WaitGroup
-var gray *image.Gray
+var wg sync.WaitGroup
+var gray image.Gray
+var ptr *image.Gray = &gray
 var img image.Image
 
 func main() {
-	// runtime.GOMAXPROCS(1)
-
-	fmt.Println("NumCPU", runtime.NumCPU())
 	fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(0))
 
 	start := time.Now()
 
 	img, _ = loadImage("The_Sun_in_high_resolution.jpg")
 
-	gray = rgbaToGray(img)
-
-	f, _ := os.Create("gray.png")
-	defer f.Close()
-	png.Encode(f, gray)
+	wg.Add(2)
+	go rgbaToGray(img)
+	go rgbaToGray(img)
+	wg.Wait()
+	createGrayImage()
 
 	t := time.Now()
 	elapsed := t.Sub(start)
@@ -50,17 +49,23 @@ func loadImage(filepath string) (image.Image, error) {
 	return img, nil
 }
 
-func rgbaToGray(img image.Image) *image.Gray {
-	// defer wg.Done()
+func rgbaToGray(img image.Image) {
+	defer wg.Done()
 	var (
 		bounds = img.Bounds()
-		gray   = image.NewGray(bounds)
+		gray2  = image.NewGray(bounds)
 	)
 	for x := 0; x < bounds.Max.X; x++ {
 		for y := 0; y < bounds.Max.Y; y++ {
 			var rgba = img.At(x, y)
-			gray.Set(x, y, rgba)
+			gray2.Set(x, y, rgba)
 		}
 	}
-	return gray
+	gray = *gray2
+}
+
+func createGrayImage() {
+	f, _ := os.Create("gray.png")
+	defer f.Close()
+	png.Encode(f, &gray)
 }
